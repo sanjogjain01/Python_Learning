61907f5f2d93d71dadb782ebf706b101502570f7
sanjogjain01 <jainsonu699@gmail.com>
2024-03-06 21:39:58 +0530

    3 march

--- DAY11_OOPS/Lab_113.py
+++ DAY11_OOPS/Lab_113.py
@@ -1,6 +1,8 @@
 # Override Concept >> when we have the same method in child with different implementation
 # Calling parent class method using super keyword
 class A:
+    a = 10
+
     def M1(self):
         print("This is M1 method of Class A")
 
@@ -11,9 +13,11 @@ class B(A):
         super().M1()  # also invoke parent class method
 
 
+
+
 ob = B()
 ob.M1()
-
+print(super(B, ob).a)  # In this way only we can access parent variable outside of any method
 
 # Accessing Variable in Child Class
 
@@ -31,5 +35,4 @@ class B(A):
 
 
 ob = B()
-ob.M1(300,400)
-
+ob.M1(300, 400)
